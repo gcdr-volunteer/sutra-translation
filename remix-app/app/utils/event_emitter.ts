51dd5d8b400740b4e5214002d748fe2e464e99ec
Terry Pan <pttdev123@gmail.com>
2023-02-10 14:51:25 +1000

    fix: tiny patch

--- remix-app/app/utils/event_emitter.ts
+++ remix-app/app/utils/event_emitter.ts
@@ -7,9 +7,12 @@ declare global {
   var __emitter: EventEmitter | undefined;
 }
 
-if (!global.__emitter) {
-  global.__emitter = new EventEmitter();
+if (process.env.NODE_ENV === 'test') {
+  emitter = new EventEmitter();
 } else {
+  if (!global.__emitter) {
+    global.__emitter = new EventEmitter();
+  }
   emitter = global.__emitter;
 }
 
