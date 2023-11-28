fba532c1110eecbcdd5ebaa3019cdd1fcdeda99f
Terry Pan <pttdev123@gmail.com>
2023-11-28 21:59:49 +1000

    feat: fix timeout issue

--- stacks/RemixStack.ts
+++ stacks/RemixStack.ts
@@ -104,6 +104,7 @@ export async function RemixStack({ stack }: StackContext) {
   const site = new RemixSite(stack, 'Site', {
     permissions: ['ses', 'es'],
     path: 'remix-app/',
+    timeout: '180 seconds',
     nodejs: {
       format: 'cjs',
     },
